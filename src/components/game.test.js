1aa88fbf52a4b406f908b2798578ca6be9ace3a3
Ngoc Tina Hoang <ngoc.tina.hoang@outlook.com>
2017-11-12 09:27:45 -0500

    one more test

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -4,5 +4,20 @@ import {shallow} from 'enzyme';
 import Game from './game.js';
 
 describe('<Game/>', ()=>{
-    
+    it('Renders without crashing', () => {
+        shallow(<Game />);
+    });
+    it('Starts a new game', ()=>{
+        const wrapper = shallow(<Game />);
+        wrapper.setState({
+            guesses: [1,2,3,4,5],
+            feedback:'Good guesses',
+            correctAnswer: -1
+        });
+        wrapper.instance().newGame();
+        expect(wrapper.state('guesses')).toEqual([]);
+        expect(wrapper.state('feedback')).toEqual('Make your guess!');
+        expect(wrapper.state('correctAnswer')).toBeGreaterThanOrEqual(0);
+        expect(wrapper.state('correctAnswer')).toBeLessThanOrEqual(100);
+    })
 })
